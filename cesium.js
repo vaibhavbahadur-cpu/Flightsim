bdc91a12869f511d3a940de8a1ead695ca0ce9df
vaibhavbahadur-cpu <vaibhav.bahadur@gmail.com>
2026-04-06 10:39:04 -0500

    Refactor FlightWorld constructor and camera settings
    Removed navViewer initialization and adjusted camera pitch.

--- cesium.js
+++ cesium.js
@@ -1,21 +1,13 @@
 export class FlightWorld {
-    constructor(containerId, navMapId, token) {
+    constructor(containerId, token) {
         Cesium.Ion.defaultAccessToken = token;
 
         this.viewer = new Cesium.Viewer(containerId, {
             terrain: Cesium.Terrain.fromWorldTerrain(),
             selectionIndicator: false,
-            infoBox: false, 
+            infoBox: false,
             baseLayerPicker: false,
             geocoder: false,
-            animation: false,
-            timeline: false
-        });
-
-        this.navViewer = new Cesium.Viewer(navMapId, {
-            sceneMode: Cesium.SceneMode.SCENE2D,
-            baseLayerPicker: false,
-            geocoder: true,
             animation: false,
             timeline: false
         });
@@ -25,7 +17,7 @@ export class FlightWorld {
             destination : Cesium.Cartesian3.fromDegrees(-97.67, 30.19, 1500),
             orientation : {
                 heading : Cesium.Math.toRadians(0.0),
-                pitch : Cesium.Math.toRadians(-30.0),
+                pitch : Cesium.Math.toRadians(-35.0),
                 roll : 0.0
             },
             duration: 4
