7045272a8acb111995dff2106011ed8288292afb
vaibhavbahadur-cpu <vaibhav.bahadur@gmail.com>
2026-04-06 11:02:15 -0500

    Refactor Cesium initialization in FlightWorld class
    Updated Cesium access token and viewer initialization to use window.Cesium.

--- cesium.js
+++ cesium.js
@@ -1,24 +1,15 @@
 export class FlightWorld {
     constructor(containerId, token) {
-        Cesium.Ion.defaultAccessToken = token;
-        this.viewer = new Cesium.Viewer(containerId, {
-            terrain: Cesium.Terrain.fromWorldTerrain(),
+        window.Cesium.Ion.defaultAccessToken = token;
+        
+        this.viewer = new window.Cesium.Viewer(containerId, {
+            terrain: window.Cesium.Terrain.fromWorldTerrain(),
             selectionIndicator: false,
             infoBox: false,
             baseLayerPicker: false,
             geocoder: false,
             animation: false,
             timeline: false
-        });
-
-        // Instant camera move to Austin
-        this.viewer.camera.setView({
-            destination: Cesium.Cartesian3.fromDegrees(-97.67, 30.19, 2000),
-            orientation: {
-                heading: Cesium.Math.toRadians(0),
-                pitch: Cesium.Math.toRadians(-30),
-                roll: 0
-            }
         });
     }
 }
