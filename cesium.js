06b6dab158782a9f99c66b063213e9a19db429d3
vaibhavbahadur-cpu <vaibhav.bahadur@gmail.com>
2026-04-06 10:34:09 -0500

    Refactor comments and maintain viewer settings

--- cesium.js
+++ cesium.js
@@ -2,18 +2,16 @@ export class FlightWorld {
     constructor(containerId, navMapId, token) {
         Cesium.Ion.defaultAccessToken = token;
 
-        // Main Viewer - Settings optimized to stop "About:Blank" errors
         this.viewer = new Cesium.Viewer(containerId, {
             terrain: Cesium.Terrain.fromWorldTerrain(),
             selectionIndicator: false,
-            infoBox: false, // Critical: Stops the sandboxed frame error
+            infoBox: false, 
             baseLayerPicker: false,
             geocoder: false,
             animation: false,
             timeline: false
         });
 
-        // 2D Nav Map
         this.navViewer = new Cesium.Viewer(navMapId, {
             sceneMode: Cesium.SceneMode.SCENE2D,
             baseLayerPicker: false,
@@ -22,7 +20,7 @@ export class FlightWorld {
             timeline: false
         });
 
-        // Trigger the descent to Austin
+        // Drop camera from space to Austin
         this.viewer.camera.flyTo({
             destination : Cesium.Cartesian3.fromDegrees(-97.67, 30.19, 1500),
             orientation : {
