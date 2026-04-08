1d426c5cda1cb341f3deb00193a124919ee24817
vaibhavbahadur-cpu <vaibhav.bahadur@gmail.com>
2026-04-07 20:39:16 -0500

    Update plane.js

--- plane/plane.js
+++ plane/plane.js
@@ -22,30 +22,27 @@ export class Boeing748 {
             }
         });
 
-        // NEW: Create the spoiler planers
-        this.createSpoiler(true);  // Left
-        this.createSpoiler(false); // Right
+        this.createSpoiler(true);  
+        this.createSpoiler(false); 
 
         this.viewer.trackedEntity = this.aircraftEntity;
-        console.log("747-8 Model Loaded Successfully with Spoilers.");
     }
 
     createSpoiler(isLeft) {
-        const xOffset = isLeft ? -15 : 15; 
+        const xOffset = isLeft ? -20 : 20; 
         const yOffset = -5; 
-        const zOffset = 2.5; 
+        const zOffset = 5.0; // High enough to clear the wing mesh
 
         this.viewer.entities.add({
             parent: this.aircraftEntity,
             position: new Cesium.Cartesian3(xOffset, yOffset, zOffset),
             box: {
-                dimensions: new Cesium.Cartesian3(5.0, 3.0, 0.05), // Thin rectangular planer
-                material: Cesium.Color.DARKGRAY.withAlpha(0.8),
+                dimensions: new Cesium.Cartesian3(12.0, 6.0, 0.2), // Big and visible
+                material: Cesium.Color.RED.withAlpha(1.0),
                 outline: true,
                 outlineColor: Cesium.Color.BLACK
             },
             orientation: new Cesium.CallbackProperty(() => {
-                // Pivot 60 degrees on the X axis when active
                 return Cesium.Quaternion.fromAxisAngle(
                     Cesium.Cartesian3.UNIT_X, 
                     Cesium.Math.toRadians(-this.spoilerAngle)
@@ -55,7 +52,6 @@ export class Boeing748 {
     }
 
     updateVisuals(physicsData) {
-        // Receives the angle from the physics engine
         this.spoilerAngle = physicsData.spoilerAngle || 0;
     }
 }
