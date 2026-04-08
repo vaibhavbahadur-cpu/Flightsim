fcabe40abb686e191608e35192a8c9bec3774508
vaibhavbahadur-cpu <vaibhav.bahadur@gmail.com>
2026-04-08 15:28:43 -0500

    Update plane.js

--- plane/plane.js
+++ plane/plane.js
@@ -4,16 +4,39 @@ export class Boeing748 {
         this.modelUri = 'https://raw.githack.com/vaibhavbahadur-cpu/7478/main/Boeing%20747-8I.glb';
         this.aircraftEntity = null;
     }
+
     spawn(lon, lat, alt) {
         const position = Cesium.Cartesian3.fromDegrees(lon, lat, alt);
         const hpr = new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(170), 0, 0);
         const orientation = Cesium.Transforms.headingPitchRollQuaternion(position, hpr);
+
         this.aircraftEntity = this.viewer.entities.add({
+            name: 'Boeing 747-8',
             position: position,
             orientation: orientation,
-            model: { uri: this.modelUri, minimumPixelSize: 128 }
+            model: { 
+                uri: this.modelUri, 
+                minimumPixelSize: 128 
+            }
         });
+
+        // Set tracking but allow mouse interaction
         this.viewer.trackedEntity = this.aircraftEntity;
     }
-    updateVisuals() {}
+
+    update(data) {
+        if (!this.aircraftEntity) return;
+
+        const position = Cesium.Cartesian3.fromDegrees(data.longitude, data.latitude, data.altitude);
+        const hpr = new Cesium.HeadingPitchRoll(
+            Cesium.Math.toRadians(data.heading),
+            Cesium.Math.toRadians(data.pitch),
+            Cesium.Math.toRadians(data.roll)
+        );
+        const orientation = Cesium.Transforms.headingPitchRollQuaternion(position, hpr);
+
+        // Directly updating properties preserves the camera track
+        this.aircraftEntity.position = position;
+        this.aircraftEntity.orientation = orientation;
+    }
 }
