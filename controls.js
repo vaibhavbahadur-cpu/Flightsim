f8f69228a71ddb04a0d1ee89198e6cc1727dbc21
vaibhavbahadur-cpu <vaibhav.bahadur@gmail.com>
2026-04-08 15:29:04 -0500

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,19 +1,14 @@
 export class FlightControls {
     constructor() {
         this.keys = {
-            ArrowUp: false,
-            ArrowDown: false,
-            ArrowLeft: false,
-            ArrowRight: false,
-            KeyA: false,
-            KeyD: false,
-            KeyB: false, // Spoiler Key
-            Space: false 
+            ArrowUp: false, ArrowDown: false,
+            ArrowLeft: false, ArrowRight: false,
+            KeyA: false, KeyD: false,
+            KeyB: false, Space: false 
         };
         this.throttle = 5;
 
         window.addEventListener('keydown', (e) => {
-            console.log("Control Detected:", e.code); // DEBUG: Check your console (F12)
             if (e.key >= '1' && e.key <= '9') this.throttle = parseInt(e.key);
             if (this.keys.hasOwnProperty(e.code)) {
                 this.keys[e.code] = true;
